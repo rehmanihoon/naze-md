3c8b261eabb21a581483055ebc9dacb6d782f1e6
rehmanihoon <rehmanihoon@gmail.com>
2024-08-18 09:42:57 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /**
-   * Create By Dika Ardnt.
-   * Recode By Naze Dev
-   * Contact Me on wa.me/6282113821188
-   * Follow https://github.com/nazedev
+   * Created By Amaan Bashir.
+   * Recode By Amaan Bashir
+   * Contact Me on wa.me/923317282386
+   * Follow https://github.com/rehmanihoon
 */
 
 const fs = require('fs')
@@ -19,12 +19,12 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['923317282386']
+global.ownernomer = "923317282386"
+global.premium = ['923317282386']
 global.packname = 'Sticker By'
-global.author = 'Naze Dev'
-global.sessionName = 'nazedev'  //jangan diganti bro nanti error
+global.author = 'Amaan Bashir'
+global.sessionName = 'amaanbashir'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
 global.jumlah = '1000000000'
@@ -34,13 +34,13 @@ global.sp = ''
 // Setting Mess
 global.mess = {
     success: '✅Done',
-    admin: 'Fitur Khusus Admin Group!',
-    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa / Chat Owner',
-    owner: 'Fitur Khusus Owner Bot',
-    group: 'Fitur Digunakan Hanya Untuk Group!',
-    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
-    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    admin: 'This Feature is only used by Group Admin!',
+    botAdmin: 'This Feature is only used by Bot Admin!',
+    premime: 'This Feature is Only For Premium Users / Chat Owner',
+    owner: 'This feature is only used by Bot Owner',
+    group: 'This Feature Only used in Group!',
+    private: 'This Feature Only used in Private Chat!',
+    bot: 'This Feature is only for Bot Number',
     wait: 'Loading...',
     error: 'Error!',
     errapi: 'Error Mungkin Apikey Tidak Valid!',
@@ -59,15 +59,15 @@ global.thumb = fs.readFileSync('./media/image/naze.jpg')
 global.faall = fs.readFileSync('./media/image/fake.jpg')
 
 // Url
-global.mygit = 'https://github.com/nazedev'
-global.myyt = 'https://youtube.com/c/Nazedev'
-global.myytv = 'https://youtu.be/FAsL-Jy4qLc'
-global.mygc = "https://chat.whatsapp.com/Cp1OJenk6Q9D9vgLjLU558"
+global.mygit = 'https://github.com/rehmanihoon/naze-md/edit/main/config.js'
+global.myyt = 'https://youtube.com/c/amaan2211'
+global.myytv = 'https://youtu.be/2211'
+global.mygc = "https://whatsapp.com/channel/0029Vag06KlLSmbUEGefRp16"
 
 // Engak Ngaruh, Belum Kepasang
-global.botname = 'NAZE BOT'
+global.botname = 'AMAAN BOT'
 global.akulaku = 'Bot By Naze'
-global.ytname = 'YT NAZE' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
+global.ytname = 'AMAAN 2211' //Kalo Ngk Punya Yt Ngak Usah Di Ganti
 
 
 let file = require.resolve(__filename)
